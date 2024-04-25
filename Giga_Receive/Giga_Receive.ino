4bf45dbbfda05e5279767d13a5746c186922128b
DaltonCrawley <144277338+DaltonCrawley@users.noreply.github.com>
2024-04-25 13:13:59 -0500

    transmission achieved

--- Giga_Receive/Giga_Receive.ino
+++ Giga_Receive/Giga_Receive.ino
@@ -1,85 +1,168 @@
+/*
+
+  WiFi UDP Send and Receive String
+
+ This sketch wait an UDP packet on localPort using a WiFi shield.
+
+ When a packet is received an Acknowledge packet is sent to the client on port remotePort
+
+ Circuit:
+
+ * WiFi shield attached
+
+ created 30 December 2012
+
+ by dlf (Metodo2 srl)
+
+ */
+
+#include <SPI.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
 
-const char* ssid = "NETGEAR32";
-const char* password = "breezybreeze113";
-const uint16_t receivePort = 4292; // Port for receiving data from ESP32_Slave
-IPAddress staticIP(192, 168, 1, 149); // statically set IP for ESP32_Slave
-const char *senderIP = "192.168.1.5"; // Set the IP address of the Arduino Giga R1 (not in use yet)
+int status = WL_IDLE_STATUS;
+char ssid[] = "NETGEAR32"; //  your network SSID (name)
+char pass[] = "breezybreeze113";    // your network password (use for WPA, or use as key for WEP)
 
-WiFiServer server(receivePort);
 
-void setup(){
-  Serial.begin(115220);
-  delay(1000);
-  Serial.println("Code Started");
+unsigned int localPort = 4292;      // local port to listen on
+
+char packetBuffer[255]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+
+WiFiUDP Udp;
+
+void setup() {
+
+  //Initialize serial and wait for port to open:
+
+  Serial.begin(115200);
+
+  while (!Serial) {
+
+    ; // wait for serial port to connect. Needed for native USB port only
+
+  }
+
+  // check for the presence of the shield:
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+
+    Serial.println("WiFi shield not present");
+
+    // don't continue:
+
+    while (true);
+
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv != "1.1.0") {
+
+    Serial.println("Please upgrade the firmware");
+
+  }
+
+  // attempt to connect to Wifi network:
+
+  while (status != WL_CONNECTED) {
+
+    Serial.print("Attempting to connect to SSID: ");
+
+    Serial.println(ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+
+    delay(10000);
+
+  }
+
+  Serial.println("Connected to wifi");
+
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+
+  // if you get a connection, report back via serial:
+
+  Udp.begin(localPort);
 }
 
 void loop() {
-  // Check for a connection to WiFi
-  while (WiFi.status() != WL_CONNECTED){
-  // Connect to WiFi if it isn't connected
-    Serial.println("Connecting to WiFi...");
-    WiFi.config(staticIP);
-    WiFi.begin(ssid, password);
-    delay(1000);
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Router Connection has been Established!");
-      // Set static IP address for Arduino Giga R1
-      printWifiStatus();
-      Serial.println("Static IP configured");
 
-      // Start server
-      server.begin();
-      Serial.println("Server started");
+  // if there's data available, read a packet
+
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize) {
+
+    Serial.print("Received packet of size ");
+
+    Serial.println(packetSize);
+
+    Serial.print("From ");
+
+    IPAddress remoteIp = Udp.remoteIP();
+
+    Serial.print(remoteIp);
+
+    Serial.print(", port ");
+
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+
+    int len = Udp.read(packetBuffer, 255);
+
+    if (len > 0) {
+
+      packetBuffer[len] = 0;
+
     }
 
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    delay(1000);
-    client = server.available();
-    Serial.println("Looking for Client...");
-  }
-  Serial.println("Client Connection Established!");
+    Serial.println("Contents:");
 
-  // Wait until the client sends data
-  while (!client.available()) {
-    Serial.println("Waiting to Recieve...");
-    delay(1000); 
-  }
-  // Read the data and process it
-  String receivedData = client.readString();
-  processReceivedData(receivedData);
-  // Send data to laptop
-  // Close the connection
-  client.stop();
-}
-}
+    Serial.println(packetBuffer);
 
-void processReceivedData(String data) {
-  Serial.print("Received: ");
-  Serial.println(data);
-  // Add your processing logic here
+    // send a reply, to the IP address and port that sent us the packet we received
+
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+
+    Udp.write(ReplyBuffer);
+
+    Udp.endPacket();
+
+  }
 }
 
 void printWifiStatus() {
+
   // print the SSID of the network you're attached to:
+
   Serial.print("SSID: ");
+
   Serial.println(WiFi.SSID());
+
   // print your WiFi shield's IP address:
-  Serial.print("LocalIP: ");
-  Serial.print(WiFi.localIP());
-  Serial.print("Subnet Mask: ");
-  Serial.println(WiFi.subnetMask());
-  Serial.print("Gateway IP: ");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("DNS 1: ");
-  Serial.println(WiFi.dnsIP(0));
-  Serial.print("DNS 2: ");
-  Serial.println(WiFi.dnsIP(1));
+
+  IPAddress ip = WiFi.localIP();
+
+  Serial.print("IP Address: ");
+
+  Serial.println(ip);
+
   // print the received signal strength:
+
   long rssi = WiFi.RSSI();
+
   Serial.print("signal strength (RSSI):");
+
   Serial.print(rssi);
+
   Serial.println(" dBm");
 }
