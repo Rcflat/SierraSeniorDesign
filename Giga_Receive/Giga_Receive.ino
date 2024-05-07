077c6e0ada14e352daeab5926f07c050af4a790d
DaltonCrawley <daltonmcrawley@gmail.com>
2024-05-07 09:28:08 -0500

    implemented lights and added speed control logic

--- Giga_Receive/Giga_Receive.ino
+++ Giga_Receive/Giga_Receive.ino
@@ -7,6 +7,7 @@
 
 JsonDocument doc;
 
+int prev_dpad; 
 int status = WL_IDLE_STATUS;
 char ssid[] = "NETGEAR32"; //  your network SSID (name)
 char pass[] = "breezybreeze113";    // your network password (use for WPA, or use as key for WEP)
@@ -88,18 +89,40 @@ void initializationWiFiStatus(){ // used in setup put here to improve readabilit
 void pinMapping(){ // for now only works with controller
   const int outputPinS1 = 2; // PWM output pin for S1
   const int outputPinS2 = 3; // PWM output pin for S2
+  const int LEDoutputPin = 9; // output pin for LED
 
   myServoS1.attach(outputPinS1); // Attaching servo to the pin
   myServoS2.attach(outputPinS2); // Attaching servo to the pin
 } 
 
 void updateValues(){ // for now only works with controller
+
   int sensorValueS1 = doc["axisY"];  //analogRead(potPin); // Read potentiometer value (-512-512)
   int sensorValueS2 = doc["axisY"];  //analogRead(potPin); // Read potentiometer value (-512-512)
-  int pulseWidthS1 = map(sensorValueS1, -512, 512, 1000, 2000); // Map potentiometer value to pulse width (1000 - 2000 microSeconds)
-  int pulseWidthS2 = map(sensorValueS2, -512, 512, 1000, 1950); // Map potentiometer value to pulse width (1000 - 2000 microSeconds)
+
+  // d-pad logic will go on client side in the future but for the preview day I am putting it on the robot to make my life simpler
+  int Servo_Ranges[][] = {{1000, 1050},{1000, 1150},{1000, 1500},{1000, 2000}}; // desired speed ranges per speed setting
+  int speed_setting = 0; // value to index the Servo_Ranges 2-dimensional array
+  int dpad_value = doc["dpad"];
+  if (dpad_value == 1 && prev_dpad != 1){ // if d-pad top button pressed shift up in range (conditional values currently filler)
+    if (speed_setting != 3){
+      speed_setting += 1;
+    }
+  }
+  else if (dpad_value == 4 && prev_dpad != 4){ // if d-pad bottom button pressed shift down in range (conditional values currently filler)
+    if (speed_setting != 0){
+      speed_setting -= 1;
+    }
+  }
+  int prev_dpad = dpad_value; // keeps track of the dpad value to help simulate a button press
+  int pulseWidthS1 = map(sensorValueS1, -512, 512, Servo_Ranges[speed_setting][0], Servo_Ranges[speed_setting][1]); // Map potentiometer value to pulse width (1000 - 2000 microSeconds)
+  int pulseWidthS2 = map(sensorValueS2, -512, 512, Servo_Ranges[speed_setting][0], Servo_Ranges[speed_setting][1]); // Map potentiometer value to pulse width (1000 - 2000 microSeconds)
   myServoS1.writeMicroseconds(pulseWidthS1); // Set servo position based on pulse width
   myServoS2.writeMicroseconds(pulseWidthS2); // Set servo position based on pulse width
+
+  int LEDValue = doc["axisRY"];
+  int LED_Adjustment = map(LEDValue, -512, 512, 0, 255);
+  analogWrite(LEDoutputPin, LED_Adjustment); // Set LED brightness based on the adjusted mapping from controller range to LED strip range
 }
 
 void printWifiStatus() {
