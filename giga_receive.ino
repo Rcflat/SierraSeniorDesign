d6550ecf46fbeb91fd26bcb653488e6b1b305640
DaltonCrawley <144277338+DaltonCrawley@users.noreply.github.com>
2024-08-21 14:25:45 -0500

    more functions ported

--- giga_receive.ino
+++ giga_receive.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
+#include <Servo.h>
 
 // Axis 0: Left Analog Stick (-1 = LEFT, 1 = RIGHT)
 // Axis 1: Left Analog Stick (-1 = UP, 1 = DOWN)
@@ -36,6 +37,7 @@ IPAddress dns(8, 8, 8, 8);               // Replaced with your DNS
 WiFiUDP udp;
 const unsigned int localPort = 8888;
 
+const int LEDoutputPin = 9; // output pin for LED
 void pinMapping(){ // for now only works with controller
   const int outputPinS1 = 2; // PWM output pin for S1
   const int outputPinS2 = 3; // PWM output pin for S2
@@ -45,6 +47,43 @@ void pinMapping(){ // for now only works with controller
   myServoS1.attach(outputPinS1); // Attaching servo to the pin
   myServoS2.attach(outputPinS2); // Attaching servo to the pin
 } 
+
+void updateValues(){ // for now only works with controller
+
+  int sensorValueS1 = doc["axisY"];  //analogRead(potPin); // Read potentiometer value (-512-512)
+  int sensorValueS2 = doc["axisY"];  //analogRead(potPin); // Read potentiometer value (-512-512)
+
+  // d-pad logic will go on client side in the future but for the preview day I am putting it on the robot to make my life simpler
+  int Servo_Ranges[4][2] = {{1450, 1500},{1400, 1550},{1200, 1750},{1000, 1950}}; // desired speed ranges per speed setting
+  int dpad_value = doc["dpad"];
+  if (dpad_value == 1 && prev_dpad != dpad_value){ // if d-pad top button pressed shift up in range (conditional values currently filler)
+    if (speed_setting != 3){
+      speed_setting += 1;
+    }
+  }
+  else if (dpad_value == 2 && prev_dpad != dpad_value){ // if d-pad bottom button pressed shift down in range (conditional values currently filler)
+    if (speed_setting != 0){
+      speed_setting -= 1;
+    }
+  }
+  
+  int prev_dpad = dpad_value; // keeps track of the dpad value to help simulate a button press
+  int pulseWidthS1 = map(sensorValueS1, -512, 512, Servo_Ranges[speed_setting][0], Servo_Ranges[speed_setting][1]); // Map potentiometer value to pulse width (1000 - 2000 microSeconds)
+  int pulseWidthS2 = map(sensorValueS2, -512, 512, Servo_Ranges[speed_setting][0], Servo_Ranges[speed_setting][1]); // Map potentiometer value to pulse width (1000 - 2000 microSeconds)
+  myServoS1.writeMicroseconds(pulseWidthS1); // Set servo position based on pulse width
+  myServoS2.writeMicroseconds(pulseWidthS2); // Set servo position based on pulse width
+
+  int LEDValue = doc["axisRY"];
+  int LED_Adjustment = map(LEDValue, -512, 512, 0, 255);
+  analogWrite(LEDoutputPin, LED_Adjustment); // Set LED brightness based on the adjusted mapping from controller range to LED strip range
+
+  Serial.print("Speed Setting: "); // print out information about sender to serial port
+  Serial.print(speed_setting);
+  Serial.print("Servo Pulse Width (1): ");
+  Serial.print(pulseWidthS1);
+  Serial.print("Servo Pulse Width (2): ");
+  Serial.println(pulseWidthS2);
+}
 
 void setup() {
   Serial.begin(115200);
