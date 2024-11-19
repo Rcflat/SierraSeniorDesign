03001b1f38510b7ae6793fb9cfa63a2f9a32c6f4
DaltonCrawley <daltonmcrawley@gmail.com>
2024-11-19 14:33:34 -0600

    fixed the code

--- Robot_Controls/arduino_code/test_code/test_code.ino
+++ Robot_Controls/arduino_code/test_code/test_code.ino
@@ -1,23 +1,22 @@
 #include <ESP32Servo.h>  // Include the ESP32 Servo library to control servo motors
 
 // Define servo objects for pan and tilt movements
-Servo panServo;
-Servo tiltServo;
+char motor1SpeedChar[20];
+char motor2SpeedChar[20];
 
-// Define pin numbers for the pan and tilt servos
-int panPin = 33;
-int tiltPin = 32;
+void SendSabertoothCommand(int motor1Speed, int motor2Speed) {
+
+  sprintf(motor1SpeedChar, "M1: %d \r\n", motor1Speed);
+  Serial2.print(motor1SpeedChar);
+  sprintf(motor2SpeedChar, "M2: %d \r\n", motor2Speed);
+  Serial2.print(motor2SpeedChar);
+}
 
 void setup() {
-  // Attach the servos to their respective pins
-  panServo.attach(panPin);  // Attach pan servo to pin 33
-  tiltServo.attach(tiltPin);  // Attach tilt servo to pin 32
+    int leftMotorValue = 1500;
+    int rightMotorValue = 1500;
 }
 
 void loop() {
-    panServo.write(1500);
-    tiltServo.write(1500);
-
-    // Small delay to stabilize the servos
-    delay(20);
+    SendSabertoothCommand(leftMotorValue, rightMotorValue);
 }
